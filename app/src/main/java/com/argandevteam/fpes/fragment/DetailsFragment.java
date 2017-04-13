d0f7c92e3f8b24a1cf05a804d14939b5b3a517e9
Marcos Stival <markcosta15@hotmail.com>
2017-04-13 13:21:34 +0200

    Updated Details Layout. Added a MapView to it so it should pin the Centre Location on the map

--- app/src/main/java/com/argandevteam/fpes/fragment/DetailsFragment.java
+++ app/src/main/java/com/argandevteam/fpes/fragment/DetailsFragment.java
@@ -4,6 +4,7 @@ package com.argandevteam.fpes.fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.NestedScrollView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -15,6 +16,14 @@ import com.argandevteam.fpes.R;
 import com.argandevteam.fpes.adapter.ReviewsAdapter;
 import com.argandevteam.fpes.model.Centre;
 import com.argandevteam.fpes.model.Review;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,13 +34,14 @@ import butterknife.ButterKnife;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DetailsFragment extends Fragment {
+public class DetailsFragment extends Fragment implements OnMapReadyCallback{
 
     Centre centre;
     ReviewsAdapter reviewsAdapter;
     ArrayList<Review> reviewsList;
     @BindView(R.id.reviewList)
     ListView lvReview;
+    MapView mapView;
 
 
     public DetailsFragment() {
@@ -50,10 +60,22 @@ public class DetailsFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_details, container, false);
         ButterKnife.bind(this, view);
+
+
         getActivity().setTitle("Detalles");
 
         ViewGroup listViewHeader = (ViewGroup) inflater.inflate(R.layout.list_view_header, lvReview, false);
         lvReview.addHeaderView(listViewHeader);
+
+        mapView = (MapView) listViewHeader.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mapView.getMapAsync(this);
 
         ViewGroup listViewFooter = (ViewGroup) inflater.inflate(R.layout.list_view_footer, lvReview, false);
         lvReview.addFooterView(listViewFooter);
@@ -77,4 +99,8 @@ public class DetailsFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
 }
